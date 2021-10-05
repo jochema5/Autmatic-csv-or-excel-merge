874f3f1d48844263f1d74ef4226009a421fa1bab
jochema5 <75548933+jochema5@users.noreply.github.com>
2021-10-05 09:12:13 +0200

    Create Automaticdatamerge.py

---
+++ Automaticdatamerge.py
@@ -0,0 +1,35 @@
+#Import the necessary libraries
+import pandas as pd
+import glob
+
+#we create the glob list of files to read, the asterisk (*) 
+# replaces any combination of characters. A question mark (?) 
+# replaces a character. If nothing is excluded, it reads all files in format
+
+#For read csv
+csv_example = glob.glob("example*.csv")
+
+#For read xlsx
+xlsx_example = glob.glob("example*xlsx")
+
+#See the table
+xlsx_example
+
+csv_example
+
+#Create the list where save the results
+list_example =[]
+
+ #Read the files using the bucle
+for i in csv_ss:
+    ss = pd.read_csv(i)
+    list_ss.append(ss)
+
+#CSee the list read
+list_ss
+
+#Concatenate the files
+df =pd.concat(list_example, ignore_index=True)
+
+#Enviamos el archivo a un csv
+df.to_csv("example.csv")
